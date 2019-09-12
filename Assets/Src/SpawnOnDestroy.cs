bd23bf3ee7a314dd39e22254c07e50991517df9c
Branxord <brandonsavanco@hotmail.com>
2019-09-12 17:28:13 -0300

    Edit: Ya quedaron bien las moonstones

--- Assets/Src/SpawnOnDestroy.cs
+++ Assets/Src/SpawnOnDestroy.cs
@@ -5,6 +5,8 @@ using System;
 
 public class SpawnOnDestroy : MonoBehaviour {
 
+    public bool identityRotation = false;
+
     [Serializable]
     public class PrefabCount {
         public GameObject prefab = null;
@@ -26,7 +28,7 @@ public class SpawnOnDestroy : MonoBehaviour {
                 GameObject newGO = Instantiate<GameObject>(
                     obj.prefab,
                     transform.position,
-                    transform.rotation);
+                    (identityRotation) ? Quaternion.identity : transform.rotation);
             }
         }
     }
