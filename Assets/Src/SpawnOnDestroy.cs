c8fce7215b6691745f45ae89e3adab9c8ef5518a
Rodrigo Corral <rcorral1989@gmail.com>
2019-09-11 22:45:28 -0300

    new: script de drop

---
+++ Assets/Src/SpawnOnDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpawnOnDestroy : MonoBehaviour {
+
+    [Serializable]
+    public class PrefabCount {
+        public GameObject prefab = null;
+        public int count = 0;
+    }
+
+
+    public PrefabCount[] objectsToSpawn;
+
+    private void OnDestroy() {
+        if(objectsToSpawn == null || objectsToSpawn.Length == 0)
+            return;
+
+        foreach(var obj in objectsToSpawn) {
+            if(obj == null || obj.count == 0 || obj.prefab == null)
+                continue;
+
+            for(int i = 0; i < obj.count; i++) {
+                GameObject newGO = Instantiate<GameObject>(
+                    obj.prefab,
+                    transform.position,
+                    transform.rotation);
+            }
+        }
+    }
+}
