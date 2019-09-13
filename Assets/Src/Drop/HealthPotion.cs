83cacdc035cf3b56c07bf91e90ec61264a41dd9a
Rodrigo Corral <rcorral1989@gmail.com>
2019-09-13 18:26:51 -0300

    new: pota de vida

---
+++ Assets/Src/Drop/HealthPotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : Drop {
+
+    public int health = 3;
+
+    protected override void OnGrab(Avatar avatar) {
+        base.OnGrab(avatar);
+        avatar.AddHealth(health);
+    }
+}
